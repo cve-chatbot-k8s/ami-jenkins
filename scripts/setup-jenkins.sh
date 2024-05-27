6f6c214ab467dab09e60b5e04f067be1c3740e7d
girish332 <kulkarni.gi@northeastern.edu>
2024-05-27 15:31:31 -0400

    Girish : Added pipeline to build packer image

--- scripts/setup-jenkins.sh
+++ scripts/setup-jenkins.sh
@@ -4,7 +4,7 @@
 sudo apt update
 
 # Install JDK 17
-sudo apt install fontconfig openjdk-17-jre
+sudo apt install openjdk-17-jre -y
 
 # Install Jenkins From Official Repository
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
@@ -13,7 +13,23 @@ echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get install jenkins -y
 
 # Start Jenkins
+sudo systemctl enable jenkins
 sudo systemctl start jenkins
+
+# Install Caddy
+sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+sudo apt update
+sudo apt install caddy
+
+# Configure Caddy
+echo ":80 {
+  reverse_proxy localhost:8080
+}" | sudo tee /etc/caddy/Caddyfile
+
+# Restart Caddy
+sudo systemctl restart caddy
