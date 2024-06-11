ec826ebc4d927224a4c2871dc9402617ed47059b
raazadarsh <raj.ada@northeastern.edu>
2024-06-10 23:22:47 -0400

    fix: installed helm in AMI

--- scripts/setup-jenkins.sh
+++ scripts/setup-jenkins.sh
@@ -124,3 +124,11 @@ sudo usermod -a -G docker jenkins
 
 sudo systemctl enable docker
 sudo systemctl start docker
+
+#install helm
+sudo apt-get update
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg >/dev/null
+sudo apt-get install apt-transport-https -y
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" |
+  sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update && sudo apt-get install helm
