738f632d4b97853feba9b2ef54dbf6cb7957577a
raazadarsh <raj.ada@northeastern.edu>
2024-06-12 12:31:42 -0400

    fix: updated multibranch to pipleline for semver

--- groovy/helm_webapp.groovy
+++ groovy/helm_webapp.groovy
@@ -46,25 +46,43 @@ multibranchPipelineJob('helm-ci-checks') {
     }
 }
 
-multibranchPipelineJob('helm-chart-semantic-versioning') {
-    branchSources {
-        github {
-            id('csye7125-helm-semver')
-            scanCredentialsId('github-credentials')
-            repoOwner('csye7125-su24-team7')
-            repository('helm-webapp-cve-processor')
-        }
-    }
+// multibranchPipelineJob('helm-chart-semantic-versioning') {
+//     branchSources {
+//         github {
+//             id('csye7125-helm-semver')
+//             scanCredentialsId('github-credentials')
+//             repoOwner('csye7125-su24-team7')
+//             repository('helm-webapp-cve-processor')
+//         }
+//     }
 
-    orphanedItemStrategy {
-        discardOldItems {
-            numToKeep(-1)
-            daysToKeep(-1)
-        }
-    }
-    factory {
-        workflowBranchProjectFactory {
-            scriptPath('Jenkinsfile_releaserc') // This is the Jenkinsfile for CI checks
+//     orphanedItemStrategy {
+//         discardOldItems {
+//             numToKeep(-1)
+//             daysToKeep(-1)
+//         }
+//     }
+//     factory {
+//         workflowBranchProjectFactory {
+//             scriptPath('Jenkinsfile_releaserc') // This is the Jenkinsfile for CI checks
+//         }
+//     }
+// }
+
+// Separate Pipeline Job for Semantic Versioning
+pipelineJob('helm-chart-semantic-versioning') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/csye7125-su24-team7/helm-webapp-cve-processor')
+                        credentials('github-credentials')
+                    }
+                    branch('main')
+                }
+            }
+            scriptPath('Jenkinsfile_releaserc') // This is the Jenkinsfile for semantic versioning
         }
     }
 }
