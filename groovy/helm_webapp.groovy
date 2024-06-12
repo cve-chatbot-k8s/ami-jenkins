91334885e834a700434f9cc1b4dbec80484055de
raazadarsh <raj.ada@northeastern.edu>
2024-06-12 00:41:38 -0400

    feat: added job for semver

--- groovy/helm_webapp.groovy
+++ groovy/helm_webapp.groovy
@@ -15,6 +15,11 @@ multibranchPipelineJob('helm-conventional-commit-check') {
             daysToKeep(-1)
         }
     }
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath('Jenkinsfile') // This is the Jenkinsfile for Conventional Commit Check
+        }
+    }
 }
 
 // Multibranch Pipeline Job for Helm-Webapp Pull Request Checks
@@ -32,6 +37,34 @@ multibranchPipelineJob('helm-ci-checks') {
         discardOldItems {
             numToKeep(-1)
             daysToKeep(-1)
+        }
+    }
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath('Jenkinsfile') // This is the Jenkinsfile for CI checks
+        }
+    }
+}
+
+multibranchPipelineJob('helm-chart-semantic-versioning') {
+    branchSources {
+        github {
+            id('csye7125-helm-semver')
+            scanCredentialsId('github-credentials')
+            repoOwner('csye7125-su24-team7')
+            repository('helm-webapp-cve-processor')
+        }
+    }
+
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(-1)
+            daysToKeep(-1)
+        }
+    }
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath('Jenkinsfile_releaserc') // This is the Jenkinsfile for CI checks
         }
     }
 }
