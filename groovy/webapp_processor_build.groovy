ce79fefdde279a45eae1fdf71a5d99634bc9e573
raazadarsh <raj.ada@northeastern.edu>
2024-06-26 16:31:33 -0400

    fix: added missing docker job for cve consumer

--- groovy/webapp_processor_build.groovy
+++ groovy/webapp_processor_build.groovy
@@ -3,11 +3,49 @@ multibranchPipelineJob('webapp-processor-docker-build-publish') {
         branchSource {
             source {
                 github {
-                    id('csye7125-webapp')
+                    id('csye7125-webapp-consumer')
                     credentialsId('github-credentials')
                     repoOwner('csye7125-su24-team7')
                     repository('webapp-cve-processor')
                     repositoryUrl('https://github.com/csye7125-su24-team7/webapp-cve-processor')
+                    configuredByUrl(true)
+                    traits {
+                        gitHubBranchDiscovery {
+                            strategyId(1)
+                            headWildcardFilter {
+                                includes('main')
+                                excludes('')
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(-1)
+            daysToKeep(-1)
+        }
+    }
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath('Jenkinsfile_docker') // This is the Jenkinsfile for building docker image
+        }
+    }
+}
+
+multibranchPipelineJob('webapp-consumer-docker-build-publish') {
+    branchSources {
+        branchSource {
+            source {
+                github {
+                    id('csye7125-webapp-consumer')
+                    credentialsId('github-credentials')
+                    repoOwner('csye7125-su24-team7')
+                    repository('webapp-cve-consumer')
+                    repositoryUrl('https://github.com/csye7125-su24-team7/webapp-cve-consumer')
                     configuredByUrl(true)
                     traits {
                         gitHubBranchDiscovery {
