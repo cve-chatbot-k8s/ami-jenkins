63272ce7af23fb2b9e4cb28f87b079c4e27f2153
raazadarsh <raj.ada@northeastern.edu>
2024-08-17 04:40:22 -0400

    feat: created jenkins job commitlint, semver and build docker

--- groovy/webapp_processor_build.groovy
+++ groovy/webapp_processor_build.groovy
@@ -151,3 +151,43 @@ multibranchPipelineJob('cve-operator-docker-build-publish') {
         }
     }
 }
+
+
+//vector
+multibranchPipelineJob('cve-vector-docker-build-publish') {
+    branchSources {
+        branchSource {
+            source {
+                github {
+                    id('csye7125-cve-vector')
+                    credentialsId('github-credentials')
+                    repoOwner('csye7125-su24-team7')
+                    repository('vector')
+                    repositoryUrl('https://github.com/csye7125-su24-team7/vector')
+                    configuredByUrl(true)
+                    traits {
+                        gitHubBranchDiscovery {
+                            strategyId(1)
+                            headWildcardFilter {
+                                includes('main')
+                                excludes('')
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(-1)
+            daysToKeep(-1)
+        }
+    }
+    factory {
+        workflowBranchProjectFactory {
+            scriptPath('Jenkinsfile_docker')   // This is the Jenkinsfile for building docker image
+        }
+    }
+}
